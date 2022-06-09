45369c825d468c68e9d9530f1acf32b4105937ad
lucykearl <104901670+lucykearl@users.noreply.github.com>
2022-06-09 17:39:54 +0100

    Completed answers in booleans.js

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,61 @@
 function negate(a) {
-  // your code here
+  return !a;
 };
 
 function both(a, b) {
-  // your code here
+  return a && b;
 };
 
 function either(a, b) {
-  // your code here
+  return a || b;
 };
 
 function none(a, b) {
-  // your code here
+  return a + b == false;
 };
 
 function one(a, b) {
-  // your code here
+  return a !== b;
 };
 
 function truthiness(a) {
-  // your code here
+  return !!a;
 };
 
 function isEqual(a, b) {
-  // your code here
+  return a === b;
 };
 
 function isGreaterThan(a, b) {
-  // your code here
+  return a > b;
 };
 
 function isLessThanOrEqualTo(a, b) {
-  // your code here
+  return a <= b;
 };
 
 function isOdd(a) {
-  // your code here
+  return Math.abs(a % 2) == 1;
 };
 
 function isEven(a) {
-  // your code here
+  return a % 2 == 0;
 };
 
 function isSquare(a) {
-  // your code here
+  return Math.sqrt(a) % 1 === 0;
 };
 
 function startsWith(char, string) {
-  // your code here
+  
 };
 
 function containsVowels(string) {
-  // your code here
+  
 };
 
 function isLowerCase(string) {
-  // your code here
+  
 };
 
 module.exports = {
